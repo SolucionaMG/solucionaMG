e105f0c1031fd329f4451e020618a12728ee9a24
Isabel <isacris.dev@outlook.com>
2025-07-27 21:52:19 -0300

    teste

---
+++ javaScript.js
@@ -0,0 +1,5 @@
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Mensagem enviada! Em breve entraremos em contato.");
+  this.reset();
+});
